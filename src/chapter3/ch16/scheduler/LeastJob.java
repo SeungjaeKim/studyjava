8e6e6847c96cf94ef09841da44ba4f230c949b50
SeungjaeKim <kimseungjae@gmail.com>
2021-03-19 22:03:28 +0900

    챕터 3 16강 과제중

--- src/chapter3/ch16/scheduler/LeastJob.java
+++ src/chapter3/ch16/scheduler/LeastJob.java
@@ -3,11 +3,11 @@ package chapter3.ch16.scheduler;
 public class LeastJob implements Scheduler {
     @Override
     public void getNextCall() {
-
+        System.out.println("상담 전화를 대기가 적은 상담원 우선 가져옵니다.");
     }
 
     @Override
     public void sendCallToAgent() {
-
+        System.out.println();
     }
 }
