7b6f4d9a62707690d6f93c488b687943a00af375
SeungjaeKim <kimseungjae@gmail.com>
2021-05-20 22:12:51 +0900

    챕터 6 8강 공부중 파일 익셉션 및 try/catch

---
+++ src/chapter6/ch08/FileExceptionHandling.java
@@ -0,0 +1,31 @@
+package chapter6.ch08;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileExceptionHandling {
+    public static void main(String[] args) {
+
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream("a.txt");
+            System.out.println("read");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println(e);
+            return;
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("finally");
+        }
+        System.out.println("end");
+    }
+}
