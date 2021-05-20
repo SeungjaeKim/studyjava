1b18f014a7ffba5c4bcd5be8d6eb9a8eebc3e41a
SeungjaeKim <kimseungjae@gmail.com>
2021-05-20 22:43:42 +0900

    챕터 6 8강 공부중 ThrowsException

---
+++ src/chapter6/ch08/ThrowsException.java
@@ -0,0 +1,25 @@
+package chapter6.ch08;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+    public Class loadClass(String fileName, String className) throws FileNotFoundException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        Class c = Class.forName(className);
+        return c;
+    }
+
+    public static void main(String[] args) {
+        ThrowsException test = new ThrowsException();
+        try {
+            test.loadClass("a.txt", "abc");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
